4eeed152b7693a1cfe305c973fe53815862c6de1
LeserraJ <leserrabusiness@gmail.com>
2023-04-27 02:17:58 +0000

    first assignment done

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,39 +1,49 @@
 const Header = () =>{
-  const Course = 'Half Stack application development'
-}
+  const course = 'Half Stack application development'
+
 return(
-    <div>
-      <h1></h1>
-      </div>
+  <div>
+    <h1>{course}</h1>
+  </div>
 )
+};
 
+const Content = (props) =>{
+  const lessons = [
+    {part: 'Fundamentals of React', exercises: 10},
+    {part: 'Using props to pass data', exercises: 7},
+    {part: 'State of a component', exercises: 14},
+  ]
+  return(
+    <div>
+      <p>{lessons[0].part} {lessons[0].exercises}</p>
+      <p>{lessons[1].part} {lessons[1].exercises}</p>
+      <p>{lessons[2].part} {lessons[2].exercises}</p>
+    </div>
+  )
+};
 
+const Total = (props) =>{
+  const exercises = [
+    {exercises: 10},
+    {exercises: 7},
+    {exercises: 14},
+  ]
 
-
+  return(
+    <div>
+      <p>Number of exercises {exercises[0].exercises + exercises[1].exercises + exercises[2].exercises}  </p>
+    </div>
+  )
+};
 
 
 const App = () => {
-  
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return (
+    return (
     <div>
-      
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+     <Header/>
+     <Content/>
+     <Total/>
     </div>
   )
 }
