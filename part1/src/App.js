1e10999e21a4e67785c17cf1251828ada83a8cd3
LeserraJ <leserrabusiness@gmail.com>
2023-04-27 01:47:35 +0000

    Git message

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,18 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+const Header = () =>{
+  const Course = 'Half Stack application development'
+}
+return(
+    <div>
+      <h1></h1>
+      </div>
+)
 
-function App() {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
-  console.log('Hello from the component')
+
+
+
+
+
+const App = () => {
+  
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
-    <div className="App">
-      <p>Hello World, it is {now.toString()}</p>
-      <p>{a} plus {b} is {a+b}</p>
+    <div>
+      
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+//3 new components Header, Content, Total, and display all in the App component
